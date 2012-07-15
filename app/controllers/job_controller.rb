668828ed3ee35b800689920523f389195b46fb60
squarism <milkfilk@gmail.com>
2012-07-15 13:56:14 -0400

    CSS, delayed_job init.  JS remote job not yet working.

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -4,16 +4,29 @@ class JobController < ApplicationController
   def new
     @pole_race = PoleRace.new
     
-    case params[:id]
-    when 'faye'
-    when 'polling'
-      @pole_race.start
-    when 'delayed_job'
-    when 'beanstalkd'
-    else
-      raise "Don't hack the Gibson.  Pass a real job type."
+    respond_with @pole_race do |format|
+      format.js {
+        case params[:id]
+        when 'delayed_job'
+          flash[:notice] = "Started a race with delayed job in JS."
+          render :js => "window.location.replace('/job/new/delayed_job');"
+        end
+      }
+      format.html {
+        case params[:id]
+        when 'faye'
+        when 'polling'
+          flash[:notice] = "Started a race with dumb procedural blocking."
+          @pole_race.start
+        when 'delayed_job'
+          #flash[:notice] = "Started a race with delayed job."
+          @pole_race.delay.start
+        when 'beanstalkd'
+        else
+          raise "Don't hack the Gibson.  Pass a real job type."
+        end
+      }
     end
-      
     #respond_with(@job_id = params[:id])
   end
   
